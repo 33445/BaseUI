d33ab04018d64634c93b215b509c4b375ea7bf44
YouLi <1665767024@qq.com>
2024-03-03 15:36:06 +0800

    添加资源

--- app/src/main/java/com/example/baseui/ui/theme/Shape.kt
+++ app/src/main/java/com/example/baseui/ui/theme/Shape.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
+val small = RoundedCornerShape(4.dp)
+val medium = RoundedCornerShape(24.dp)
+
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
--- app/src/main/java/com/example/baseui/ui/theme/Type.kt
+++ app/src/main/java/com/example/baseui/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.baseui.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.baseui.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +28,89 @@ val Typography = Typography(
     )
     */
 )
+val nunitoSansFamily = FontFamily(
+    Font(R.font.nunitosans_light, FontWeight.Light),
+    Font(R.font.nunitosans_semibold, FontWeight.SemiBold),
+    Font(R.font.nunitosans_bold, FontWeight.Bold)
+)
+
+val h1 = TextStyle(
+    fontSize = 18.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.Bold
+)
+val h2 = TextStyle(
+    fontSize = 14.sp,
+    letterSpacing = 0.15.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.Bold
+)
+val subtitle1 = TextStyle(
+    fontSize = 16.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.Light
+)
+val body1 = TextStyle(
+    fontSize = 14.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.Light
+)
+val body2 = TextStyle(
+    fontSize = 12.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.Light
+)
+val button = TextStyle(
+    fontSize = 14.sp,
+    letterSpacing = 1.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.SemiBold,
+    color = white
+)
+val caption = TextStyle(
+    fontSize = 12.sp,
+    fontFamily = nunitoSansFamily,
+    fontWeight = FontWeight.SemiBold
+)
+
+val bloomTypoGraphy = Typography(
+    h1 = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    h2 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Bold
+    ),
+    subtitle1 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
+    body2 = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.Light
+    ),
+    button = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 1.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.SemiBold,
+        color = white
+    ),
+    caption = TextStyle(
+        fontSize = 12.sp,
+        fontFamily = nunitoSansFamily,
+        fontWeight = FontWeight.SemiBold
+    )
+)
+
